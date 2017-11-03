dd34365c21dabb27294abfc3e25f8fc7caf11a2f
Ali Ravanchi <>
2017-11-02 17:25:07 -0700

    Created a fresh project

---
+++ BinanceSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BinanceSwift
+//
+//  Created by Ali Ravanchi on 11/2/17.
+//  Copyright © 2017 Cardinal Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
